b8f5f7610da5f4edd89d2332da47cbb2aa5fde91
Josué Ávalos <aezequiel56@gmail.com>
2024-07-25 13:37:26 -0600

    Logica del main para construir los objetos

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,46 @@
+import Contrato.Contratado;
+import Contrato.Ordinario;
+import profesor.Profesor;
+import Materia.Materia;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Materia materia = new Materia("M01", "Matematicas");
+        materia.incluir();
+        materia.eliminar();
+        materia.modificar();
+        materia.reporte1();
+        materia.reporte2();
+        System.out.println("*--------------------------------------------------*");
+        System.out.println("\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Profesor profesor = new Profesor("Josue", "Avalos", "Tiempo completo");
+        profesor.incluir();
+        profesor.eliminar();
+        profesor.modificar();
+        profesor.reporte1();
+        profesor.reporte2();
+        System.out.println("*--------------------------------------------------*");
+        System.out.println("\n");
+
+
+        Ordinario ordinario = new Ordinario("Gabriela", "Alas", "Tiempo completo", 5);
+        ordinario.incluir();
+        ordinario.eliminar();
+        ordinario.modificar();
+        ordinario.reporte1();
+        ordinario.reporte2();
+        System.out.println("*--------------------------------------------------*");
+        System.out.println("\n");
+
+        Contratado contratado = new Contratado("Alison", "Becker", "Tiempo completo", "01/01/2021", "01/01/2022");
+        contratado.incluir();
+        contratado.eliminar();
+        contratado.modificar();
+        contratado.reporte1();
+        contratado.reporte2();
+
     }
 }
